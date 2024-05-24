b4f065054b16a8a2acce9b7ca36f256383333e56
Vartika Gupta <88293497+vartika2302@users.noreply.github.com>
2024-05-25 03:00:39 +0530

    Create template.cpp

---
+++ template.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define YES cout<<"YES"<<endl
+#define yes cout<<"yes"<<endl
+#define Yes cout<<"Yes"<<endl
+#define NO cout<<"NO"<<endl
+#define no cout<<"no"<<endl
+#define No cout<<"No"<<endl
+
+using namespace std;
+
+static bool compare(int a,int b) {
+	return a>b;
+}
+
+void solve() {
+	
+}
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL),cout.tie(NULL);
+	int tc;
+	cin>>tc;
+	while(tc--) {
+		solve();
+	}
+	return 0;
+}
